91488b19cdb8dfc5551c4c67c1c626fcad4c5415
Rosaamelia Cordova <rosacordova1310@gmail.com>
2023-03-15 02:58:59 -0700

    Every Picture Project

--- studio2/script.js
+++ studio2/script.js
@@ -1,91 +1,87 @@
 (function () {
 
-//Image Scrolling Caresol
-    "use strict";
-    console.log("reading js");
-    
-    window.addEventListener('load', function () {
-        'use strict';
-        const sliderContent = document.querySelector('.a');
-    
-        // How wide is the original set of images?
-        const sliderWidth = sliderContent.offsetWidth;
-    
-        // clone the set of images and assign them the class name of '.b'
-        const cloned = sliderContent.cloneNode(true);
-        cloned.className = "b";
-    
-        // add the clone to the DOM
-        document.querySelector('.slider').appendChild(cloned);
-    
-        //get the :root element
-        let root = document.querySelector(':root');
-    
-        // set the end of the left position based on the width of the slider
-        const endLeftPos = `-${sliderWidth}px`;    
-        root.style.setProperty('--sliderwidth', endLeftPos);
-    
-        //Add the animate class to start animating the slider
-        document.querySelector('.slider').classList.add("animate");
-    });
+//Slideshow on Interval
+const myImages = ['laksa-stall.jpg','laksa-table.jpg','laksa-close.jpg'];
 
-//Slideshow (Advanced)
 let currentImage = 0;
-const myphotos = ["l", "image2.jpg", "image3.jpg", "image4.jpg", "image5.jpg"];
 
-const container = document.getElementById('content');
-const nextBtn = document.getElementById('next');
-const prevBtn = document.getElementById('previous');
-
-nextBtn.addEventListener('click', function (event) {
-    event.preventDefault();
-
+const slide = document.getElementById('myimage');
+setInterval(
+function nextPhoto(){
     currentImage++;
-    if (currentImage > myphotos.length - 1) { currentImage = 0; }
 
-    swapImage();
-});
-
-prevBtn.addEventListener('click', function (event) {
-    event.preventDefault();
-
-    currentImage--;
-    if (currentImage < 0) { currentImage = myphotos.length - 1; }
-
-    swapImage();
-});
-
-function swapImage() {
-    const newSlide = document.createElement('img');
-    newSlide.src = `slides/${myphotos[currentImage]}`;
-    newSlide.className = "fadeinimg";
-    container.appendChild(newSlide);
-
-    if (container.children.length > 2) {
-        container.removeChild(container.children[0]);
+    if(currentImage > myImages.length-1) {
+        currentImage = 0;
     }
-}
+
+    slide.src = `images/${myImages[currentImage]}`;
+}, 4000)
+
 
 
 //Overlays
 
 
-//Overlay 1 - Here
-document.querySelector('#office').addEventListener('click', function (event) {
-    event.preventDefault();
-    document.getElementById('laksa-overlay').className = 'showing';
-});
+//Overlay 1 - Here (Laksa)
+    document.querySelector('#office').addEventListener('click', function (event) {
+        
+        document.getElementById('laksa-overlay').className = 'showing';
+        document.getElementById('homepage').className = 'hidden';
+    });
 
-document.querySelector('.close').addEventListener('click', function (event) {
-    event.preventDefault();
-    document.getElementById('overlay').className = 'hidden';
-});
+    document.querySelector('.close').addEventListener('click', function (event) {
+        event.preventDefault();
+        document.getElementById('laksa-overlay').className = 'hidden';
+        document.getElementById('homepage').className = 'home';
+    });
 
-document.addEventListener('keydown', function (event) {
-    if (event.key === 'Escape') {
-        document.getElementById('overlay').className = 'hidden';
-    }
-});
+    document.addEventListener('keydown', function (event) {
+        if (event.key === 'Escape') {
+            document.getElementById('laksa-overlay').className = 'hidden';
+            document.getElementById('homepage').className = 'home';
+        }
+    });
+
+//Overlay 2 - Or (Whoa)
+    document.querySelector('#dog').addEventListener('click', function (event) {
+        event.preventDefault();
+        document.getElementById('whoa-overlay').className = 'showing';
+        document.getElementById('homepage').className = 'hidden';
+    });
+
+    document.querySelector('#whoa-overlay .close').addEventListener('click', function (event) {
+        event.preventDefault();
+        document.getElementById('whoa-overlay').className = 'hidden';
+        document.getElementById('homepage').className = 'home';
+    });
+
+    document.addEventListener('keydown', function (event) {
+        if (event.key === 'Escape') {
+            document.getElementById('whoa-overlay').className = 'hidden';
+            document.getElementById('homepage').className = 'home';
+        }
+    });
+
+
+//Overlay 3 - There (Tree)
+    document.querySelector('#pointing').addEventListener('click', function (event) {
+        
+        document.getElementById('tree-overlay').className = 'showing';
+        document.getElementById('homepage').className = 'hidden';
+    });
+
+    document.querySelector('#tree-overlay .close').addEventListener('click', function (event) {
+        event.preventDefault();
+        document.getElementById('tree-overlay').className = 'hidden';
+        document.getElementById('homepage').className = 'home';
+    });
+
+    document.addEventListener('keydown', function (event) {
+        if (event.key === 'Escape') {
+            document.getElementById('tree-overlay').className = 'hidden';
+            document.getElementById('homepage').className = 'home';
+        }
+    });
 
 
 
